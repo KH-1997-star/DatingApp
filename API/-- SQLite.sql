3504a0efad29523f159105704486889a8f8db5af
Khaireddine Krichene <Khaireddine.krichene@saphir.local>
2023-11-08 17:38:27 +0100

    End of section 2

---
+++ API/-- SQLite.sql
@@ -0,0 +1,11 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1,"Bob");
+
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (2,"Tom");
+
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (3,"Jane");
