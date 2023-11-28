853f0e1d99f9e4b6f55615c222247e3e844367b6
Khaireddine Krichene <Khaireddine.krichene@saphir.local>
2023-11-28 10:33:02 +0100

    End of section 6

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,39 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+   ;
+  @Output() cancelRegister = new EventEmitter();
+  
+  model: any = {};
+
+  constructor(public accountService: AccountService, private toastr: ToastrService) {}
+
+  ngOnInit(): void {
+     
+  }
+
+  register() {
+    this.accountService.register(this.model).subscribe({
+      next: response => {
+       
+        this.cancel();
+      },
+      error: error => console.log(error),
+       //this.toastr.error(error.error),
+      complete: () => console.log('register completed'),
+    })
+  }
+
+  cancel() {
+    console.log('cancelled'); 
+    this.cancelRegister.emit(false);
+  }
+
+}
